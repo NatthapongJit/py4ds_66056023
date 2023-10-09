294d6db0f7af5ef051c0aabff802f6625610bbf6
NatthapongJit <66056023@kmitl.ac.th>
2023-10-09 21:57:09 +0700

    fix 24

--- src/ex24.py
+++ src/ex24.py
@@ -16,5 +16,10 @@ def get_time_every_15_min():
     Returns:
         str: The generated time string.
     """
-    # TODO : complete this
-    pass
+    # FIX : complete this
+    # Loop over am and pm:
+
+    for meridiem in ['am', 'pm']:
+        for hour in ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']:
+            for minutes in ['00', '15', '30', '45']:
+                print(hour + ':' + minutes + ' ' + meridiem)
