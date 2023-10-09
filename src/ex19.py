5ffea365356f7be7d59b6a3e72fbb390d2c61b21
NatthapongJit <66056023@kmitl.ac.th>
2023-10-09 20:49:39 +0700

    fix 19

--- src/ex19.py
+++ src/ex19.py
@@ -3,14 +3,35 @@ Exercise 19 : Password Generator
 """
 import random
 
-LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'    # 26
-UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'    # 26
-NUMBERS = '1234567890'                          # 10
-SPECIAL = '~!@#$%^&*()_+'                       # 13
+LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'    # 26 characters
+UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'    # 26 characters
+NUMBERS = '1234567890'                          # 10 characters
+SPECIAL = '~!@#$%^&*()_+'                       # 13 characters
 
 ALL_CHARS = LOWER_LETTERS + UPPER_LETTERS + NUMBERS + SPECIAL
 
+def gen_password(l):
+    """
+    Generates a password of at least 12 characters including at least one lowercase, one uppercase,
+    one digit, and one special character.
 
-def gen_password(param):
-    # TODO : complete this
-    pass
+    Parameters:
+        l (int): Desired length of the password.
+
+    Returns:
+        str: The generated password.
+    """
+    # FIX : complete this
+    if l < 12:
+        l = 12
+
+    pw = []
+
+    pw.append(LOWER_LETTERS[random.randint(0, 25)])
+    pw.append(UPPER_LETTERS[random.randint(0, 25)])
+    pw.append(NUMBERS[random.randint(0, 9)])
+    pw.append(SPECIAL[random.randint(0, 12)])
+    while len(pw) < l:
+        pw.append(ALL_CHARS[random.randint(0, 74)])
+    random.shuffle(pw)
+    return ''.join(pw)
